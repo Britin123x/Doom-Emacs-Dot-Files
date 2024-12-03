0bf33e37b2ef78e2563163f48e5bf3af2cc22887
Britin Hanna <britinhanna@outlook.com>
2024-12-03 14:12:32 -0600

    Final Post-Install Version

--- config.el
+++ config.el
@@ -4,7 +4,6 @@
       user-mail-address "britin1@ksu.edu")
 
 (setq doom-theme 'doom-ir-black)
-(set-face-attribute 'default nil :height 160)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 (setq display-line-numbers-type 'relative)
@@ -73,52 +72,3 @@
     (smtpmail-smtp-user     . "britin1@ksu.edu")
     (mu4e-compose-signature . "---\nBest, \nBritin Hanna\n Kansas State University - Electrical Engineering - Junior \n College of Engineering Student Senator \n KSU Department of Mathematics Office Worker \n (620) 212-9533"))
   t)
-
-(use-package! eaf
-  :load-path "~/.elisp/emacs-application-framework"
-  :init
-  :custom
-  (eaf-browser-continue-where-left-off t)
-  (eaf-browser-enable-adblocker t)
-  (browse-url-browser-function 'eaf-open-browser) ;; Make EAF Browser my default browser
-  :config
-  (defalias 'browse-web #'eaf-open-browser)
-
-  (require 'eaf-file-manager)
-  (require 'eaf-music-player)
-  (require 'eaf-image-viewer)
-  (require 'eaf-camera)
-  (require 'eaf-demo)
-  (require 'eaf-airshare)
-  (require 'eaf-terminal)
-  (require 'eaf-markdown-previewer)
-  (require 'eaf-video-player)
-  (require 'eaf-vue-demo)
-  (require 'eaf-file-sender)
-  (require 'eaf-pdf-viewer)
-  (require 'eaf-mindmap)
-  (require 'eaf-netease-cloud-music)
-  (require 'eaf-jupyter)
-  (require 'eaf-org-previewer)
-  (require 'eaf-system-monitor)
-  (require 'eaf-rss-reader)
-  (require 'eaf-file-browser)
-  (require 'eaf-browser)
-  (require 'eaf-org)
-  (require 'eaf-mail)
-  (require 'eaf-git)
-  (when (display-graphic-p)
-    (require 'eaf-all-the-icons))
-
-  (require 'eaf-evil)
-  (define-key key-translation-map (kbd "SPC")
-    (lambda (prompt)
-      (if (derived-mode-p 'eaf-mode)
-          (pcase eaf--buffer-app-name
-            ("browser" (if  (string= (eaf-call-sync "call_function" eaf--buffer-id "is_focus") "True")
-                           (kbd "SPC")
-                         (kbd eaf-evil-leader-key)))
-            ("pdf-viewer" (kbd eaf-evil-leader-key))
-            ("image-viewer" (kbd eaf-evil-leader-key))
-            (_  (kbd "SPC")))
-        (kbd "SPC")))))
